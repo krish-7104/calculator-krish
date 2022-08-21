ce0c0d8291ea90e6c232980741ece5a90ba66e31
Krish Jotaniya <jotaniyakrish07@gmail.com>
2022-08-21 16:13:32 +0530

    Updating 1st Stage

---
+++ index.js
@@ -0,0 +1,22 @@
+let storeArea = document.getElementById("hidden");
+let inputArea = document.getElementById("inputId");
+
+storeArea.innerText = " ";
+
+const clickEventHandler = (id) => {
+  if (id !== "back" && id !== "clear" && id !== "equal") {
+    storeArea.innerText += id;
+    inputArea.value = storeArea.innerText;
+  } else if (id == "back") {
+    storeArea.innerText = storeArea.innerText.slice(0, -1);
+    inputArea.value = storeArea.innerText;
+  } else if (id == "clear") {
+    storeArea.innerText = "";
+    inputArea.value = storeArea.innerText;
+  } else if (id == "equal") {
+    let toCount = document.getElementById("inputId");
+    toCount = eval(toCount.value);
+    storeArea.innerText = toCount;
+    inputArea.value = storeArea.innerText;
+  }
+};
